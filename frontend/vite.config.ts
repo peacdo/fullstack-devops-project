abf98e0878923774453f54343ae68ee853913d63
peacdo <gorkemozyilmaz@outlook.com>
2025-01-23 13:23:51 +0300

    mime config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
   server: {
     port: 5173,
     proxy: {
