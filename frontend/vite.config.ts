5f19ea9e30ca0fea09e97c25e1e8613137f5c247
peacdo <gorkemozyilmaz@outlook.com>
2025-01-23 15:16:55 +0300

    other stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,15 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1000,
     assetsDir: 'assets',
     rollupOptions: {
       output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
         assetFileNames: 'assets/[name]-[hash][extname]',
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
